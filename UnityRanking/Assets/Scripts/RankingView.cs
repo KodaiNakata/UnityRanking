82d9c87b348cebe871c9ae8e0619c6e385806712
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2021-07-09 21:34:36 +0900

    TypeをTimeに変更してみた

--- UnityRanking/Assets/Scripts/RankingView.cs
+++ UnityRanking/Assets/Scripts/RankingView.cs
@@ -7,8 +7,13 @@ public class RankingView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Type == Number �̏ꍇ
-        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(100);
+        //// Type == Number �̏ꍇ
+        //naichilab.RankingLoader.Instance.SendScoreAndShowRanking(100);
+
+        // Type == Time �̏ꍇ
+        var millsec = 1;
+        var timeScore = new System.TimeSpan(0, 0, 0, 0, millsec);
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(timeScore);
     }
 
     // Update is called once per frame
